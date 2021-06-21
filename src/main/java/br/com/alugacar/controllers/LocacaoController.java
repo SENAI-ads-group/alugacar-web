f3ffda98375f740b2a65f8da942a063f0737a0ab
Patrick-Ribeiro <patrickribeiro.rb@gmail.com>
2021-06-21 19:30:06 -0300

    post adicionar locação

--- src/main/java/br/com/alugacar/controllers/LocacaoController.java
+++ src/main/java/br/com/alugacar/controllers/LocacaoController.java
@@ -1,14 +1,18 @@
 package br.com.alugacar.controllers;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import br.com.alugacar.annotations.AutenticacaoNecessaria;
+import br.com.alugacar.entidades.Locacao;
 import br.com.alugacar.entidades.enums.StatusVeiculo;
 import br.com.alugacar.services.LocacaoService;
 import br.com.alugacar.services.VeiculoService;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 
 @Path("locacoes")
@@ -28,5 +32,16 @@ public class LocacaoController {
 	@Get
 	public void adicionar() {
 		result.include("veiculoList", veiculoService.getStatus(StatusVeiculo.DISPONIVEL_PARA_ALUGAR));
+	}
+
+	public List<Locacao> listar() {
+		return locacaoService.getTodas();
+	}
+
+	@AutenticacaoNecessaria
+	@Post
+	public Locacao cadastrar(Locacao locacao) {
+		locacao = locacaoService.inserir(locacao);
+		return locacao;
 	}
 }
--- src/main/java/br/com/alugacar/entidades/enums/StatusLocacao.java
+++ src/main/java/br/com/alugacar/entidades/enums/StatusLocacao.java
@@ -1,5 +1,21 @@
 package br.com.alugacar.entidades.enums;
 
 public enum StatusLocacao {
-	PENDENTE_DE_RETIRADA, EM_PROCESSO, PENDENTE_DE_DEVOLUCAO;
+	VEICULO_RESERVADO("Veículo Reservado"), EM_ANDAMENTO("Em Andamento"), DATA_DEVOLUCAO_EXPIRADA("Expirada"),
+	FINALIZADA("Finalizada");
+
+	private String nomeFormatado;
+
+	private StatusLocacao(String nomeFormatado) {
+		this.nomeFormatado = nomeFormatado;
+	}
+
+	public String getNomeFormatado() {
+		return nomeFormatado;
+	}
+
+	public void setNomeFormatado(String nomeFormatado) {
+		this.nomeFormatado = nomeFormatado;
+	}
+
 }
